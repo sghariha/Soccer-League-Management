f5e88088d67b8da3c5c3727ea09dac9961dc3801
Shreeman <sghariha@ucsd.edu>
2017-12-05 16:44:45 -0800

    Created app.js. Add all JS to this file

--- register.js
+++ register.js
@@ -1,9 +1,98 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('existing-team-signup').style.display = "flex";
+var config = {
+    apiKey: "AIzaSyBig_htEJTY6jZ1LpuT6DvXnrqrXaa2heY",
+    authDomain: "cse134b-527c4.firebaseapp.com",
+    databaseURL: "https://cse134b-527c4.firebaseio.com",
+    projectId: "cse134b-527c4",
+    storageBucket: "cse134b-527c4.appspot.com",
+    messagingSenderId: "46669459351"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
 
-	document.getElementById('mySelect').value = "player";
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('new-team-signup').style.display = "none";
+    var team = [];
+    var query = database.ref("teams").orderByKey();
+	query.once("value")
+	.then(function(snapshot) {
+    	snapshot.forEach(function(childSnapshot) {
+      		team.push(childSnapshot.key);
+      		console.log(team);
+      		var x = document.getElementById("jointeam");
+			var option = document.createElement("option");
+			option.text = childSnapshot.key;
+			option.value = childSnapshot.key;
+			x.add(option, x[0]);
+  		});
+	});
 });
 
+function showExistingTeam() {
+	document.getElementById('new-team-signup').style.display = "none";
+	document.getElementById('existing-team-signup').style.display = "block";
+	if(document.getElementById('nav_new').classList.contains("active")) {
+		document.getElementById('nav_new').classList.remove("active");
+	}
+	if(!document.getElementById('nav_existing').classList.contains("active")) {
+		document.getElementById('nav_existing').classList.add("active");
+	}
+}
+
+function showNewTeam() {
+	document.getElementById('existing-team-signup').style.display = "none";
+	document.getElementById('new-team-signup').style.display = "block";
+	if(document.getElementById('nav_existing').classList.contains("active")) {
+		document.getElementById('nav_existing').classList.remove("active");
+	}
+	if(!document.getElementById('nav_new').classList.contains("active")) {
+		document.getElementById('nav_new').classList.add("active");
+	}
+}
+
+function registerExisting() {
+	if(document.getElementById('username').value && document.getElementById('email').value && 
+	document.getElementById('password').value && document.getElementById('confirmpassword').value) {
+		if(document.getElementById('password').value === document.getElementById('confirmpassword').value) {
+			firebase.auth().createUserWithEmailAndPassword(document.getElementById('email').value, 
+			document.getElementById('password').value).catch(function(error) {
+  				var errorCode = error.code;
+  				var errorMessage = error.message;
+  				document.getElementById("warning").innerHTML = "Ensure email is valid and if password is at least 6 characters!";
+  				return false;
+			});
+			var storedEmail = document.getElementById('email').value.replace('.', '').replace('@', '');
+			firebase.database().ref('users').update({[storedEmail] : document.getElementById('jointeam').value})
+		}
+		else {
+			document.getElementById("warning").innerHTML = "Passwords do not match!";
+		}
+	}
+}
+
+function registerNew() {
+	if(document.getElementById('username').value && document.getElementById('email').value && 
+	document.getElementById('password').value && document.getElementById('confirmpassword').value && 
+	document.getElementById('team-new').value) {
+		if(document.getElementById('password').value === document.getElementById('confirmpassword').value) {
+			firebase.auth().createUserWithEmailAndPassword(document.getElementById('email').value, 
+			document.getElementById('password').value).then(function(result) {
+				var storedEmail = document.getElementById('email').value.replace('.', '').replace('@', '');
+				firebase.database().ref('users').update({[storedEmail] : document.getElementById('team-new').value});
+				firebase.database().ref('teams/' + document.getElementById('team-new').value).update({teamname : document.getElementById('team-new').value});
+			}).catch(function(error) {
+  				var errorCode = error.code;
+  				var errorMessage = error.message;
+  				document.getElementById("warning").innerHTML = "Ensure email is valid and if password is at least 6 characters!";
+  				return false;
+			});
+		}
+		else {
+			document.getElementById("warning").innerHTML = "Passwords do not match!";
+		}
+	}
+}
+
+/*
 function change(obj) {
 	var select = obj;
 	var selected = select.options[select.selectedIndex].value;
@@ -49,4 +138,4 @@ function register() {
 	}
 	window.location = "login.html"
 	return false;
-}
+} */
