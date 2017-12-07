289ffdf77a07b805db966eab8042358c3f0375fb
Shreeman <sghariha@ucsd.edu>
2017-12-07 12:15:03 -0800

    working on firestore in register

--- app.js
+++ app.js
@@ -29,6 +29,32 @@ var config = {
   var database = firebase.database();
 var name, email, photoUrl, uid, emailVerified, team;
 
+var db  =firebase.firestore();
+/*
+firebase.firestore().enablePersistence()
+  .then(function() {
+      // Initialize Cloud Firestore through firebase
+      db = firebase.firestore();
+      writeToFirestore();
+  })
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });*/
+
+function writeToFirestore() {
+db.collection('users').doc('first').set({
+  user: 'three',
+  name: 'One'
+});
+}
 
 /* start of stats javascript */
 function loadStats() {
